2b832bd80a84d7ec99f7f06dbec2e4e8b51b2fb6
benspier <benspier@gmail.com>
2018-11-01 21:16:49 -0400

    revise event listeners

--- main.js
+++ main.js
@@ -2,15 +2,24 @@ const box = document.getElementById("text-box");
 const button = document.getElementById("button");
 const list = document.getElementById("list")
 
-button.addEventListener("click", function () {
-    const listItem = document.createElement("li");
-    list.appendChild(listItem);
-    listItem.textContent = box.value;
-    box.value = "";
+document.addEventListener("keypress", e => {
+    if (e.which === 13) {
+        addToList()
+    }
 })
 
-// function addToList() {
-//     butextContent("click", function () {
-//         console.log(textBox.value)
-//     })
-// }
+button.addEventListener("click", () => {
+    addToList()
+})
+
+const addToList = () => {
+    const li = document.createElement("li");
+    const liCheckbox = document.createElement("input")
+    const liLabel = document.createElement("label")
+    liCheckbox.type = "checkbox";
+    liLabel.innerText = box.value;
+    box.value = "";
+    list.appendChild(li);
+    li.appendChild(liCheckbox)
+    li.appendChild(liLabel)
+}
