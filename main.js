2524b2fa9c8886f46a493f0d53a3f20dfb53845d
benspier <benspier@gmail.com>
2018-11-11 22:01:22 -0500

    add packages, revise main.js

--- main.js
+++ main.js
@@ -1,39 +1,42 @@
-const box = document.querySelector("#text-box");
-const addButton = document.querySelector("#add-button");
-const completedButton = document.querySelector("#completed-button");
-const clearButton = document.querySelector("#clear-button");
-const list = document.querySelector("list")
+const textBox = document.querySelector('#text-box');
+const addButton = document.querySelector('#add-button');
+const clearCompletedButton = document.querySelector('#clear-completed-button');
+const list = document.querySelector('ul');
 
-//add item with enter key
-document.addEventListener("keypress", e => {
-  if (box.value !== "") {
+//add item with return key
+document.addEventListener('keypress', e => {
+  if (textBox.value !== '') {
     if (e.which === 13) {
       addToList();
     }
   }
 });
 
-//add item with button
-addButton.addEventListener("click", () => {
-  if (box.value !== "") {
+//add-item button
+addButton.addEventListener('click', () => {
+  if (textBox.value !== '') {
     addToList();
   }
 });
 
-//remove completed items button
-// completedButton.addEventListener("click", () => {
-
-// })
+//clear-completed button
+clearCompletedButton.addEventListener('click', () => {
+  while ((list.querySelector('input').checked = true)) {
+    list.querySelector('input:checked + label').remove();
+    list.querySelector('input:checked').remove();
+  }
+});
 
 //function
-const addToList = () => {
-  const li = document.createElement("li");
-  const liCheckbox = document.createElement("input");
-  const liLabel = document.createElement("label");
-  liCheckbox.type = "checkbox";
-  liLabel.innerText = box.value;
-  box.value = "";
+function addToList() {
+  const li = document.createElement('li');
+  const liCheckbox = document.createElement('input');
+  const liLabel = document.createElement('label');
+  liCheckbox.type = 'checkbox';
+  liCheckbox.dataset.checkbox = 'unchecked';
+  liLabel.innerText = textBox.value;
+  textBox.value = '';
   list.appendChild(li);
   li.appendChild(liCheckbox);
   li.appendChild(liLabel);
-};
+}
