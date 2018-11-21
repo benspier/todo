50e5894792ecb5fc927ad9a9611ef19f226f54c7
benspier <benspier@gmail.com>
2018-11-21 16:31:11 -0500

    click li to check

--- main.js
+++ main.js
@@ -3,15 +3,26 @@ const addButton = document.querySelector('#add-button');
 const clearCompletedButton = document.querySelector('#clear-completed-button');
 const clearAllButton = document.querySelector('#clear-all-button');
 const list = document.querySelector('ul');
+let n = '0';
 
+//load page with cursor in text field
 textBox.focus();
 
 list.addEventListener('click', e => {
-  if (e.target.className === 'checked') {
-    e.target.parentNode.classList.remove('checked');
-    if (e.target.parentNode.classList === 'checked')
-      e.target.classList.remove('checked');
-  } else e.target.parentNode.classList.add('checked');
+  if (
+    e.target.tagName === 'LI' &&
+    e.target.querySelector('input').checked !== 'true'
+  ) {
+    e.target.classList.toggle('done');
+    e.target.querySelector('input').checked = 'true';
+  }
+  if (
+    e.target.tagName === 'LABEL' &&
+    e.target.parentNode.querySelector('input').checked !== 'true'
+  ) {
+    e.target.parentNode.classList.toggle('done');
+    e.target.parentNode.querySelector('input').checked = 'true';
+  }
 });
 
 //add item with return key
@@ -44,6 +55,7 @@ clearAllButton.addEventListener('click', () => {
   array.forEach(function(listItem) {
     listItem.remove();
   });
+  n = '0';
 });
 
 //function
@@ -52,10 +64,12 @@ function addToList() {
   const input = document.createElement('input');
   const label = document.createElement('label');
   input.type = 'checkbox';
-  input.dataset.checkbox = 'unchecked';
   label.innerText = textBox.value;
   textBox.value = '';
   list.appendChild(li);
   li.appendChild(input);
   li.appendChild(label);
+  input.setAttribute('id', n);
+  label.setAttribute('for', n);
+  n = parseInt(Number(n) + 1);
 }
