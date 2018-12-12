1665eebba262fcecf08f263cb2de7a99ca0ee588
benspier <benspier@gmail.com>
2018-12-12 16:12:01 -0500

    refactor, add iife

--- main.js
+++ main.js
@@ -1,89 +1,91 @@
-const body = document.querySelector('body');
-const textBoxInput = body.querySelector('#text-box');
-const addButton = body.querySelector('#add-button');
-const clearButtons = body.querySelector('#clear-buttons-div');
-const clearDoneButton = clearButtons.querySelector('#clear-done-button');
-const clearAllButton = clearButtons.querySelector('#clear-all-button');
-const list = body.querySelector('ul');
-let n = 0;
+(function() {
+  const body = document.querySelector('body');
+  const textBoxInput = body.querySelector('#text-box');
+  const addButton = body.querySelector('#add-button');
+  const clearButtons = body.querySelector('#clear-buttons-div');
+  const clearDoneButton = clearButtons.querySelector('#clear-done-button');
+  const clearAllButton = clearButtons.querySelector('#clear-all-button');
+  const list = body.querySelector('ul');
+  let n = 0;
 
-//load page with cursor in text field
-textBoxInput.focus();
+  textBoxInput.focus();
 
-list.addEventListener('click', e => {
-  const element = e.target.closest('li');
-  element.firstChild.checked = true;
-  element.classList.toggle('done');
-  console.log(element);
-});
+  //event listeners
+  textBoxInput.addEventListener('keypress', e => {
+    if (textBoxInput.value !== '') {
+      if (e.which === 13) {
+        addToList();
+      }
+    }
+  });
 
-//add item with return key
-textBoxInput.addEventListener('keypress', e => {
-  if (textBoxInput.value !== '') {
-    if (e.which === 13) {
+  addButton.addEventListener('click', () => {
+    if (textBoxInput.value !== '') {
       addToList();
     }
-  }
-});
-
-//'add item' button
-addButton.addEventListener('click', () => {
-  if (textBoxInput.value !== '') {
-    addToList();
-  }
-});
-
-//'clear done' button
-clearDoneButton.addEventListener('click', () => {
-  const array = list.querySelectorAll('input:checked');
-  array.forEach(function(listItem) {
-    listItem.parentNode.remove();
   });
-  if (!list.querySelector('li')) {
-    restoreDefaultStyle();
-  }
-  textBox.focus();
-});
 
-//'clear all' button
-clearAllButton.addEventListener('click', () => {
-  const array = list.querySelectorAll('li');
-  array.forEach(function(listItem) {
-    listItem.remove();
+  clearDoneButton.addEventListener('click', () => {
+    const array = list.querySelectorAll('input:checked');
+    array.forEach(function(listItem) {
+      listItem.parentNode.remove();
+    });
+    if (!list.querySelector('li')) {
+      restoreDefaultStyle();
+    }
+    textBox.focus();
   });
-  restoreDefaultStyle();
-});
 
-//functions
-function addToList() {
-  const li = document.createElement('li');
-  const input = document.createElement('input');
-  const label = document.createElement('label');
-  if (!list.querySelector('li')) {
-    listStyle();
+  clearAllButton.addEventListener('click', () => {
+    const array = list.querySelectorAll('li');
+    array.forEach(function(listItem) {
+      listItem.remove();
+    });
+    restoreDefaultBodyStyle();
+  });
+
+  list.addEventListener('click', e => {
+    checkBox(e);
+  });
+
+  //functions
+  function addToList() {
+    const li = document.createElement('li');
+    const input = document.createElement('input');
+    const label = document.createElement('label');
+    if (!list.querySelector('li')) {
+      changeBodyStyle();
+    }
+    list.appendChild(li);
+    li.appendChild(input);
+    li.appendChild(label);
+    input.setAttribute('id', n);
+    label.setAttribute('for', n);
+    input.type = 'checkbox';
+    label.innerText = textBoxInput.value;
+    textBoxInput.value = '';
+    n += 1;
+    textBoxInput.focus();
   }
-  input.type = 'checkbox';
-  label.innerText = textBoxInput.value;
-  textBoxInput.value = '';
-  list.appendChild(li);
-  li.appendChild(input);
-  li.appendChild(label);
-  input.setAttribute('id', n);
-  label.setAttribute('for', n);
-  n += 1;
-  textBoxInput.focus();
-}
 
-function listStyle() {
-  body.classList.toggle('responsive-margin');
-  clearButtons.style.visibility = 'visible';
-  textBoxInput.placeholder = 'add something else...';
-}
+  function checkBox(e) {
+    const element = e.target.closest('li');
+    element.firstChild.checked = true;
+    element.classList.toggle('done');
+    console.log(element);
+  }
 
-function restoreDefaultStyle() {
-  body.classList.toggle('responsive-margin');
-  clearButtons.style.visibility = 'hidden';
-  textBoxInput.placeholder = 'start a list...';
-  n = 0;
-  textBoxInput.focus();
-}
+  function changeBodyStyle() {
+    body.classList.toggle('responsive-margin');
+    clearButtons.style.visibility = 'visible';
+    textBoxInput.placeholder = 'add something else...';
+  }
+
+  function restoreDefaultBodyStyle() {
+    body.classList.toggle('responsive-margin');
+    clearButtons.style.visibility = 'hidden';
+    textBoxInput.placeholder = 'start a list...';
+    n = 0;
+    textBoxInput.focus();
+  }
+})();
